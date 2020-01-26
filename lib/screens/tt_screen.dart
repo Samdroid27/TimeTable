dc8f9da842f1498d13f0cb8c0aa8f01211076fb8
shivam <shivamagr2711@gmail.com>
2020-01-27 02:02:34 +0530

    Fixed slot Controller

--- lib/screens/tt_screen.dart
+++ lib/screens/tt_screen.dart
@@ -45,7 +45,7 @@ class _TTScreenState extends State<TTScreen> {
                   context,
                   MaterialPageRoute(
                     builder: (context) =>
-                      MyApp()
+                      SecondScreen(receivedNotification.title)
                   ),
                 );
               },
@@ -57,7 +57,7 @@ class _TTScreenState extends State<TTScreen> {
     selectNotificationSubject.stream.listen((String payload) async {
       await Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => MyApp()),
+        MaterialPageRoute(builder: (context) => TTScreen()),
       );
     });
     checkNotify();
@@ -197,3 +197,42 @@ class _TTScreenState extends State<TTScreen> {
   }
 
 
+class SecondScreen extends StatefulWidget {
+  SecondScreen(this.payload);
+
+  final String payload;
+
+  @override
+  State<StatefulWidget> createState() => SecondScreenState();
+}
+
+class SecondScreenState extends State<SecondScreen> {
+  String _payload;
+  @override
+  void initState() {
+    super.initState();
+    _payload = widget.payload;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${(_payload ?? '')}'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Class in 10 min'),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go back!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/timetable_display.dart
+++ lib/widgets/timetable_display.dart
@@ -8,8 +8,9 @@ class TimeTableDisplay extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-  void modifySlotDialog(BuildContext context, int hr, String column) {
-    final _slotController = TextEditingController();
+  void modifySlotDialog(
+      BuildContext context, int hr, String column, String course) {
+    final _slotController = TextEditingController(text: course);
     showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -42,51 +43,68 @@ class TimeTableDisplay extends StatelessWidget {
     return FutureBuilder(
       future: Provider.of<TTProvider>(context).fetchAndSetSlot(),
       builder: (ctx, snapshot) => Consumer<TTProvider>(
-        builder: (context, ttVal, _) =>  SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: DataTable(
-              columns: [
-                DataColumn(label: Text('time')),
-                DataColumn(label: Text('Monday')),
-                DataColumn(label: Text('Tuesday')),
-                DataColumn(label: Text('Wednesday')),
-                DataColumn(label: Text('Thursday')),
-                DataColumn(label: Text('Friday')),
-                DataColumn(label: Text('Saturday')),
-              ],
-              rows: ttVal.items
-                  .map((item) => DataRow(cells: [
-                        DataCell(Text('${item.hr}')),
-                        DataCell(Text(item.mon),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'mon')),
-                        DataCell(Text(item.tue),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'tue')),
-                        DataCell(Text(item.wed),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'wed')),
-                        DataCell(Text(item.thu),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'thu')),
-                        DataCell(Text(item.fri),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'fri')),
-                        DataCell(Text(item.sat),
-                            
-                            onTap: () =>
-                                modifySlotDialog(context, item.hr, 'sat')),
-                      ]))
-                  .toList(),
-            ),
+        builder: (context, ttVal, _) => SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: [
+              DataColumn(label: Text('time')),
+              DataColumn(label: Text('Monday')),
+              DataColumn(label: Text('Tuesday')),
+              DataColumn(label: Text('Wednesday')),
+              DataColumn(label: Text('Thursday')),
+              DataColumn(label: Text('Friday')),
+              DataColumn(label: Text('Saturday')),
+            ],
+            rows: ttVal.items
+                .map((item) => DataRow(cells: [
+                      DataCell(Text('${item.hr}')),
+                      DataCell(Text(item.mon),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'mon',
+                                item.mon,
+                              )),
+                      DataCell(Text(item.tue),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'tue',
+                                item.tue,
+                              )),
+                      DataCell(Text(item.wed),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'wed',
+                                item.wed,
+                              )),
+                      DataCell(Text(item.thu),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'thu',
+                                item.thu,
+                              )),
+                      DataCell(Text(item.fri),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'fri',
+                                item.fri,
+                              )),
+                      DataCell(Text(item.sat),
+                          onTap: () => modifySlotDialog(
+                                context,
+                                item.hr,
+                                'sat',
+                                item.sat,
+                              )),
+                    ]))
+                .toList(),
           ),
         ),
-      
+      ),
     );
   }
 }
